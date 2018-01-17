a2ecf6d6d1e8e5c9c0c6073454e41ce9cc24178a
User <User@IqmalAzhari>
2018-01-17 10:00:32 +0800

    commit2

---
+++ src/projek5/classTest1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projek5;
+
+/**
+ *
+ * @author User
+ */
+public class classTest1 {
+    
+    public static void main(String[] args){
+        int x = 30;
+        
+        if( x < 20){
+            System.out.print("This is if statement");
+}else{
+            System.out.print("This is else statement");
+        }
+}
+}
